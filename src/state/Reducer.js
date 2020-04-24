d6b52a9f5e0f212a9eb8bbd7c3cde829e0004251
Babatunde <babatundea15@gmail.com>
2020-04-24 07:59:32 +0100

    setup initial state and reducer

--- src/state/Reducer.js
+++ src/state/Reducer.js
@@ -1,10 +1,46 @@
-import { ADD_FEATURE, REMOVE_FEATURE } from "./Type";
+import { ADD_FEATURE, BUY_ITEM, REMOVE_FEATURE } from "./Type";
 
 const initialState = {
-  feature: {},
-};
+    additionalCosts: 0,
+    car: {
+      price: 26395,
+      name: "2019 Ford Mustang",
+      image:
+        "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: "V-6 engine", price: 1500 },
+      { id: 2, name: "Racing detail package", price: 1500 },
+      { id: 3, name: "Premium sound system", price: 500 },
+      { id: 4, name: "Rear spoiler", price: 250 }
+    ]
+  };
 
-const Reducer = (state = initialState, action)=>{
+export function carReducer  (car = initialState.car, action){
+    switch (action.type) {
+        case BUY_ITEM:
+            return{
+                ...car, features:[...car.features, action.payload]
+            }
+    
+        default:
+            return state;
+    }
 
 }
-export default Reducer;
+
+export function additionalFeatureReducer(additionalFeatures = initialState.additionalFeatures, action) {
+    switch (action.type) {
+        case ADD_FEATURE:
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
+
+export function additionalCostReducer(additionalCosts = initialState.additionalCosts, action) {
+}
+
